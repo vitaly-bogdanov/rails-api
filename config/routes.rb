47d3c2b4b4d1c57eb91a7952fc52d730542c82f7
Vitaly <kaliummati@gmail.com>
2020-05-01 13:32:04 +0900

    рефакторинг контроллера сессии

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   root 'posts#index'
+  namespace :api do
+    namespace :v1 do
+      scope :posts do # CRUD posts
+        get '/', to: 'posts#index'
+        post '/', to: 'posts#create'
+        patch '/:id', to: 'posts#update'
+        delete '/:id', to: 'posts#destroy'
+      end
+      scope :sessions do # auth
+        get '/logged-in', to: 'sessions#logged_in'
+      end
+    end
+    namespace :v2 do # auth v2
+    end
+  end
 
   post   '/posts',      to: 'posts#create'
   post   '/update/:id', to: 'posts#update'
