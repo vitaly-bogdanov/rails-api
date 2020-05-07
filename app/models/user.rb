9bb700f3bf38abfed95d20c9c0364bc9b08ba3e2
Vitaly <kaliummati@gmail.com>
2020-05-08 03:42:49 +0900

    небольшие правки в моделе user связанные с валидацией

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,18 @@ class User < ApplicationRecord
 
   validates :name, 
     presence:   { message: 'Поле обязательно для заполнения' }, 
-    uniqueness: { message: 'Пользователь с данным именем уже существует' }
+    uniqueness: { message: 'Пользователь с данным именем уже существует' },
+    length: { 
+      minimum: 5, 
+      maximum: 15
+    }
+
+  validates :password, 
+    presence: true,
+    length: { 
+      minimum: 4, 
+      maximum: 8
+    }
 
   validates :password_confirmation, presence: true
   validates :access, presence: true
