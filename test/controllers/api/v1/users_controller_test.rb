01473d7794ad4ce764d45c55fa71a6719d346cab
Vitaly <kaliummati@gmail.com>
2020-05-08 01:29:14 +0900

    добавленны небольшие измениения в тесты

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should ajax user create' do
+    post(api_v1_create_user_url, params: { name: 'Stella', password: '12345', passwordConfirmation: '12345' }, xhr: true)
+    assert_response :success
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
+  # test "попытка создать post с соблюдением правил валидации" do
+  #   post = Post.new(title: 'New Post', description: 'New Description', body: 'New Body New Body', image: )
   #   assert true
   # end
 end
