0b73ee8c14d1a9bae70eb4b9d519211873fc06aa
Vitaly <kaliummati@gmail.com>
2020-06-08 00:17:13 +0900

    добавленны seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+posts = [
+  { title: 'title 0', description: 'description 0', body: 'body 0' },
+  { title: 'title 1', description: 'description 1', body: 'body 1' },
+  { title: 'title 2', description: 'description 2', body: 'body 2' },
+  { title: 'title 3', description: 'description 3', body: 'body 3' }
+]
+
+posts.each do |post|
+  Post.create title: post[:title],
+              description: post[:description],
+              body: post[:body]
+end
